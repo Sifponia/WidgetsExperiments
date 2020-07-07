1fac1749b5251a853dbe11c54c78ef92c085d214
Sifponia <diegomauricio15@hotmail.com.com>
2020-07-08 00:06:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/widgetsexperiments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.widgetsexperiments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
